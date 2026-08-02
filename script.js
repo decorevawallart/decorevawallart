c8e9166cd61c881390573904d4ff328d1583fa58
decorevawallart <gauravjain0293@gmail.com>
2026-08-02 15:42:11 +0530

    Initial DECOREVA website

---
+++ script.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll(".card img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+    img.onclick = function () {
+        lightbox.style.display = "flex";
+        lightboxImg.src = this.src;
+    };
+});
+
+closeBtn.onclick = function () {
+    lightbox.style.display = "none";
+};
+
+lightbox.onclick = function(e){
+    if(e.target===lightbox){
+        lightbox.style.display="none";
+    }
+};
